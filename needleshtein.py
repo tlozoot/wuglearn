17284398c34d4808a998410787590b7239ba22b8
Jon Levine <tlozoot@gmail.com>
2010-11-05 18:16:08 -0400

    some code organization

--- needleshtein.py
+++ needleshtein.py
@@ -1,16 +1,5 @@
 #! /usr/bin/env python
 
-class Paradigm:
-  
-  def __init__(self, line):
-    word1, word2 = line.strip().split("\t")
-    print word1
-    
-  def needleman(self):
-    pass
-    
-  def levenshtein(self):
-    pass
-    
+from lib.paradigm import *   
 
-paradigms = map(lambda l: Paradigm(l), open("pairs.txt").readlines())
+paradigms = map(lambda l: Paradigm(l), open("lib/pairs.txt").readlines())
