094c6fd6ac98b460cdc9137b6ec96c4328e8b93a
Jon Levine <tlozoot@gmail.com>
2010-11-05 18:16:20 -0400

    really, some code organization

---
+++ lib/paradigm.py
@@ -0,0 +1,13 @@
+# The paradigm module--contains the paradigm class
+
+class Paradigm:
+  
+  def __init__(self, line):
+    word1, word2 = line.strip().split("\t")
+    print word1
+    
+  def needleman(self):
+    pass
+    
+  def levenshtein(self):
+    pass
