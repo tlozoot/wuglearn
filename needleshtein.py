134e4085bd0445ce63a0af60fe68325eb226a9a1
Jon Levine <tlozoot@gmail.com>
2010-11-05 16:37:20 -0400

    trying to figure out python classes. naptime.

--- needleshtein.py
+++ needleshtein.py
@@ -1,1 +1,16 @@
 #! /usr/bin/env python
+
+class Paradigm:
+  
+  def __init__(self, line):
+    word1, word2 = line.strip().split("\t")
+    print word1
+    
+  def needleman(self):
+    pass
+    
+  def levenshtein(self):
+    pass
+    
+
+paradigms = map(lambda l: Paradigm(l), open("pairs.txt").readlines())
