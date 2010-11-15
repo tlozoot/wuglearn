79e4638d257426eceb01b04f9b1b31c5ed85e117
Jon Levine <tlozoot@gmail.com>
2010-11-15 16:12:41 -0500

    futzing

---
+++ data/remove_apos.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+import re
+
+f = open("plurals.txt", 'r+')
+
+def check_theta(line):
+  word1, word2 = line.strip().split("\t")
+  return not bool(re.search('T$', word1))
+  
+f.write("\n".join(filter(check_theta, f.read().replace("'", '').split("\n"))))
