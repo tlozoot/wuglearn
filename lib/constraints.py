666d2031fd1ecb26f64e152566dd49eef0588d88
Jon Levine <tlozoot@gmail.com>
2010-11-16 23:45:17 -0500

    moved constraints

---
+++ lib/constraints.py
@@ -0,0 +1,31 @@
+##### CONSTRAINTS.PY #####
+
+# Contains a class definition for Constraint
+# Plus, a bunch of actual constraint functions
+
+class Constraint:
+  def __init__(self, func):
+    self.func = func
+    
+  
+  
+# FAITHFULNESS CONSTRAINTS
+
+def id_voice_s1(form1, form2):
+  pass
+  
+def id_voice_root(form1, form2):
+  pass
+
+def id_voice_affix(form1, form2):
+  pass
+  
+  
+  
+# MARKEDNESS CONSTRAINTS
+
+def no_long_vowels(form):
+  pass
+
+def no_long_vowels_before_f(form):
+  pass
