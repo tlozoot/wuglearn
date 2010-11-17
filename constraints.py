4df746d37fdc8110a765699d9475cad04075f036
Jon Levine <tlozoot@gmail.com>
2010-11-16 23:37:47 -0500

    sketched some constraints

--- constraints.py
+++ constraints.py
@@ -1,2 +1,14 @@
-def asdf():
+def id_voice_s1(form1, form2):
+  pass
+  
+def id_voice_root(form1, form2):
+  pass
+
+def id_voice_affix(form1, form2):
+  pass
+  
+def no_long_vowels(form):
+  pass
+
+def no_long_vowels_before_f(form):
   pass
--- learner.py
+++ learner.py
@@ -4,3 +4,5 @@
 from lib import feature
 from lib import paradigm
 
+# Read in a file from somewhere in data-
+# This part will change; we want to be pretty agnostic about how these things are formatted, at least for now
