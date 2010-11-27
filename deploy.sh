09b7565e4f86ecdcb34c3bcca5e1e6068e9202b9
Jon Levine <tlozoot@gmail.com>
2010-11-27 15:51:36 -0500

    testing new deploy for phonetics.fas

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 #! /bin/sh
 
-ssh -T jon-levine.com <<\EOI
+ssh -T dailyoperations@phonetics.fas.harvard.edu <<\EOI
 
 cd wuglearn/
 git checkout .
 git pull
-ps -e | grep python | egrep -o '^[0-9]*' | xargs kill
-nohup python server.py > nohup.out 2> nohup.err < /dev/null & 
 exit
 
 EOI
