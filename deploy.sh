6c989c95816673fe3356c5bb46495f805e2c7011
Jon Levine <tlozoot@gmail.com>
2010-11-27 05:14:19 -0500

    still testing deploy and nohup

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,7 @@ cd wuglearn/
 git checkout .
 git pull
 ps -e | grep python | egrep -o '^[0-9]*' | xargs kill
-nohup python server.py &
-
+nohup python server.py > nohup.out 2> nohup.err < /dev/null & 
 exit
 
 EOI
